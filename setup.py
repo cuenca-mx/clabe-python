54f052ccb3e12993e808192506a2bc34f99e4184
Matin Tamizi <matin@cuenca.com>
2018-10-04 15:50:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='clabe',
-    version='0.1',
+    version='0.1.1',
     author='Cuenca',
     author_email='dev@cuenca.com',
     description='Validate and generate the control digit of a CLABE in Mexico',
