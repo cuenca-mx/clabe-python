7dc4be51e658fa0ac508dbc76ad172cc0cab9f8a
LuisBrime <luisbrimeg@gmail.com>
2020-07-31 09:40:08 -0500

    change requirements installs to a better config

--- setup.py
+++ setup.py
@@ -5,15 +5,6 @@ import setuptools
 version = SourceFileLoader('version', 'clabe/version.py').load_module()
 
 
-test_requires = [
-    'pytest==5.4.3',
-    'pytest-cov==2.10.0',
-    'black==19.10b0',
-    'isort==5.0.4',
-    'flake8==3.8.3',
-    'mypy==0.782',
-]
-
 with open('README.md', 'r') as f:
     long_description = f.read()
 
@@ -31,9 +22,6 @@ setuptools.setup(
     include_package_data=True,
     package_data=dict(clabe=['py.typed']),
     install_requires=['pydantic>=1.4,<2.0'],
-    setup_requires=['pytest-runner'],
-    tests_require=test_requires,
-    extras_require=dict(test=test_requires),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
